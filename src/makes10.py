e79851348c95a4edb89463bebe4b0a48798db6e7
Karolina - Karina <hryhorchuk.karolina@chnu.edu.ua>
2024-02-22 22:03:43 +0200

    mean_.done

--- src/makes10.py
+++ src/makes10.py
@@ -1,6 +1,6 @@
 def makes10(a, b):
   # Type your code
-  if a == int and b == int and a + b == 10 :
+  if a + b == 10 or a == 10 or b == 10 and type(a) == int and type(b) == int:
     return True
   else:
     return False
