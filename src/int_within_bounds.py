e2b1f32ca27aca45c346f2b6291a12c51025f00d
Karolina - Karina <hryhorchuk.karolina@chnu.edu.ua>
2024-02-22 21:39:44 +0200

    int_within_bounds.done

--- src/int_within_bounds.py
+++ src/int_within_bounds.py
@@ -1,3 +1,6 @@
 def int_within_bounds(number, lower_bound, upper_bound):
   # Type your code
-  return 
+  if number >= lower_bound < upper_bound and type(number)==int:
+    return True
+  else:
+    return False
