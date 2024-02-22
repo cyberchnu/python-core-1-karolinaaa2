99f76c6d0a78e59c18df078d2533c7664d4abba5
Karolina - Karina <hryhorchuk.karolina@chnu.edu.ua>
2024-02-22 21:12:55 +0200

    calculate.done

--- src/calculate_fuel.py
+++ src/calculate_fuel.py
@@ -1,22 +1,5 @@
 def calculate_fuel(distance):
-  # Type your code
-  # Перевірка, чи введена вірна відстань
-  if not isinstance(distance, (int, float)) or distance <= 0:
-    print(" відстань (10)")
-    return None
-
-  # Розрахунок обсягу палива (при умові, що потрібно 10 разів більше палива, ніж відстань)
-  fuel_amount = distance * 10
-
-  # Перевірка, щоб обсяг палива був не менше 100 літрів
-  if fuel_amount < 100:
+  if distance < 10:
     return 100
   else:
-    return fuel_amount
-
-  # Приклад виклику функції
-
-
-distance = 20
-result = calculate_fuel(distance)
-print(result)
+    return (distance * 10)
