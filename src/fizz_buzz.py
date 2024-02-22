57ad77e9719e928c6c3ef9ee2914f0e973c04e13
Karolina - Karina <hryhorchuk.karolina@chnu.edu.ua>
2024-02-22 22:06:09 +0200

    fizz_buzz.done

--- src/fizz_buzz.py
+++ src/fizz_buzz.py
@@ -1,11 +1,11 @@
 def fizz_buzz(param):
   #Type your code here
-  if param % 3 :
-    return 'Fizz'
-  elif param % 5 :
-    return 'Buzz'
-  elif param % 3 and param % 5 :
-    return 'FizzBuzz'
+  if param % 3 == 0 and param % 5 == 0:
+    result = "FizzBuzz"
+  elif param % 3 == 0:
+    result = "Fizz"
+  elif param % 5 == 0:
+    result = "Buzz"
   else:
     result = str(param)
     return result
